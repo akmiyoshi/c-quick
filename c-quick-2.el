fa0ab4ee5a99dde1502809cebc2a0e4c79223ffe
akmiyoshi <akio.miyoshi@gmail.com>
2014-05-19 03:52:02 +0900

    (c-quick-mark-sexp) の実装が決定

--- c-quick-2.el
+++ c-quick-2.el
@@ -15,8 +15,8 @@
 (global-set-key (kbd "C-w")        'c-quick-kill-region)
 (global-set-key (kbd "C-M-\\")     'c-quick-indent-region)
 (global-set-key (kbd "<C-delete>") 'c-quick-delete-region)
-(global-set-key (kbd "C-M-SPC")    'c-quick-mark-sexp1)
-(global-set-key (kbd "C-M-@")      'c-quick-mark-sexp2)
+(global-set-key (kbd "C-M-SPC")    'c-quick-mark-sexp)
+(global-set-key (kbd "C-M-@")      'c-quick-mark-sexp)
 
 (defvar *c-quick-ding* t)
 
@@ -191,30 +191,12 @@
   (interactive)
   (c-quick-operate-on-region-or-sexp #'kill-region))
 
-(defun c-quick-mark-sexp1 ()
+(defun c-quick-mark-sexp ()
   (interactive)
   (if (eq last-command this-command)
       (c-quick-forward-sexp)
     (set-mark (point))
     (c-quick-forward-sexp)))
-
-(defun c-quick-mark-sexp2 ()
-  (interactive)
-  (let ((opoint (point)))
-    (cond
-     ((eq last-command this-command)
-      (goto-char (region-end))
-      (c-quick-forward-sexp)
-      (set-mark (point))
-      (c-quick-recenter)
-      (goto-char opoint)
-      )
-     (t
-      (let ((opoint (point)))
-        (c-quick-forward-sexp)
-        (set-mark (point))
-        (c-quick-recenter)
-        (goto-char opoint))))))
 
 (provide 'c-quick-2)
 ;;; c-quick-2.el ends here
