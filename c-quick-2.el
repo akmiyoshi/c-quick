9198dc4ca2a138d525af30db1f596e2200022ffd
akmiyoshi <akio.miyoshi@gmail.com>
2014-05-19 07:56:46 +0900

    改行位置の修正、コメント(参考文献)の修正

--- c-quick-2.el
+++ c-quick-2.el
@@ -186,7 +186,8 @@
         (let ((opoint (point)))
           (c-quick-backward-sexp)
           (c-quick-count-lines opoint (point))))
-       ((looking-at "\\(\\s-*\\)\\(\\sw\\|\\s_\\|\\s(\\|\\s<\\|\\s\"\\|\\s'\\)")
+       ((looking-at
+         "\\(\\s-*\\)\\(\\sw\\|\\s_\\|\\s(\\|\\s<\\|\\s\"\\|\\s'\\)")
         (goto-char (match-end 1))
         (let ((opoint (point)))
           (c-quick-forward-sexp)
@@ -256,11 +257,11 @@
 
 ;; [参考文献]
 ;;
-;; 34.2.1 構文クラス一覧
-;; http://www.geocities.co.jp/SiliconValley-Bay/9285/ELISP-JA/elisp_565.html
-;;
 ;; 3.1 Integer Basics
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Integer-Basics.html
 ;;
 ;; 28.19 The Window Start and End Positions
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Window-Start-and-End.html
+;;
+;; 34.2.1 構文クラス一覧
+;; http://www.geocities.co.jp/SiliconValley-Bay/9285/ELISP-JA/elisp_565.html
