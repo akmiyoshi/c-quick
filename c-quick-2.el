1a8807d1e56397cac777be398e311b822ee48655
akmiyoshi <akio.miyoshi@gmail.com>
2014-05-24 01:00:43 +0900

    c-quick.el に syntax.el を統合

--- c-quick-2.el
+++ c-quick-2.el
@@ -1,5 +1,5 @@
 ;; -*- coding: utf-8 -*-
-;;; c-quick-2.el --- Intelligent Cursor Movement for GNU Emacs
+;;; c-quick-2.el --- Intelligent Cursor Movement for GNU Emacs/XEmacs
 ;;
 ;; Copyright (C) 1993-2014  akmiyoshi
 ;;
@@ -22,6 +22,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (require 'find-func)
+(require 'etags)
 
 
 (global-set-key (kbd "<down>")     'c-quick-down-key)
@@ -262,8 +263,10 @@
                     (setq comment-begin
                           (c-quick-find-comment-beginning (point)))))
         (goto-char comment-begin)))
-     ((cq-looking-back "\\s-") (while (cq-looking-back "\\s-") (backward-char)))
-     ((cq-looking-back "\\s<") (while (cq-looking-back "\\s<") (backward-char)))
+     ((cq-looking-back "\\s-")
+      (while (cq-looking-back "\\s-") (backward-char)))
+     ((cq-looking-back "\\s<")
+      (while (cq-looking-back "\\s<") (backward-char)))
      ((cq-looking-back "\n")
       (backward-char)
       (while (and (bolp) (cq-looking-back "\n")
@@ -277,8 +280,10 @@
     (cond
      ((bobp) (c-quick-ding))
      ((cq-looking-back "\\s(") (c-quick-ding))
-     ((cq-looking-back "\\s-") (while (cq-looking-back "\\s-") (backward-char)))
-     ((cq-looking-back "\\s<") (while (cq-looking-back "\\s<") (backward-char)))
+     ((cq-looking-back "\\s-")
+      (while (cq-looking-back "\\s-") (backward-char)))
+     ((cq-looking-back "\\s<")
+      (while (cq-looking-back "\\s<") (backward-char)))
      ((cq-looking-back "\n") nil)
      (t (let* ((opoint (point))
                (within-comment (c-quick-within-comment (point)))
@@ -293,12 +298,12 @@
 (defun c-quick-within-string (pos)
   (save-excursion
     (goto-char pos)
-    (let ((parsed (syntax-ppss)))
+    (let ((parsed (cq-syntax-ppss)))
       (if (nth 3 parsed) (nth 8 parsed) nil))))
 
 (defun c-quick-forward-within-string ()
   (let ((opoint (point))
-        (parsed (syntax-ppss))
+        (parsed (cq-syntax-ppss))
         beg end)
     (save-excursion
       (setq beg (nth 8 parsed))
@@ -311,7 +316,7 @@
 
 (defun c-quick-backward-within-string ()
   (let ((opoint (point))
-        (parsed (syntax-ppss))
+        (parsed (cq-syntax-ppss))
         beg end)
     (save-excursion
       (setq beg (nth 8 parsed))
@@ -325,7 +330,7 @@
 (defun c-quick-within-comment (pos)
   (save-excursion
     (goto-char pos)
-    (let ((parsed (syntax-ppss)))
+    (let ((parsed (cq-syntax-ppss)))
       (if (not (nth 4 parsed))
           nil
         (goto-char (nth 8 parsed))
@@ -425,10 +430,6 @@
   (interactive)
   (c-quick-operate-on-region-or-sexp #'delete-region))
 
-;(defun c-quick-indent-region ()
-;  (interactive)
-;  (c-quick-operate-on-region-or-sexp #'indent-region))
-
 (defun c-quick-indent-region ()
   (interactive)
   (c-quick-operate-on-region-or-sexp
@@ -485,10 +486,10 @@
 
 (defun c-quick-jump-to-function-or-variable ()
   (interactive)
-  (let* ((func-name (cq-find-tag-default))
+  (let* ((func-name (find-tag-default))
          (interned (intern func-name)))
     (cond
-     ((c-quick-built-in-function-p interned)
+     ((cq-built-in-function-p interned)
       (error "%s is a built-in function" interned))
      ((fboundp interned)
       (find-function interned))
@@ -496,40 +497,13 @@
       (find-variable interned))
      (t (error "%s is not a lisp function nor a user variable" interned)))))
 
-(defun c-quick-built-in-function-p (symbol)
+(defun cq-built-in-function-p (symbol)
   ;; (require 'cl) (assert (symbolp symbol))
   (if (not (fboundp symbol))
       nil
     (subrp
      (symbol-function
       (cq-find-function-advised-original symbol)))))
-
-(defun cq-find-tag-default ()
-  "Determine default tag to search for, based on text at point.
-If there is no plausible default, return nil."
-  (let (from to bound)
-    (when (or (progn
-                ;; Look at text around `point'.
-                (save-excursion
-                  (skip-syntax-backward "w_") (setq from (point)))
-                (save-excursion
-                  (skip-syntax-forward "w_") (setq to (point)))
-                (> to from))
-              ;; Look between `line-beginning-position' and `point'.
-              (save-excursion
-                (and (setq bound (line-beginning-position))
-                     (skip-syntax-backward "^w_" bound)
-                     (> (setq to (point)) bound)
-                     (skip-syntax-backward "w_")
-                     (setq from (point))))
-              ;; Look between `point' and `line-end-position'.
-              (save-excursion
-                (and (setq bound (line-end-position))
-                     (skip-syntax-forward "^w_" bound)
-                     (< (setq from (point)) bound)
-                     (skip-syntax-forward "w_")
-                     (setq to (point)))))
-      (buffer-substring-no-properties from to))))
 
 (defun cq-find-function-advised-original (func)
   "Return the original function symbol of an advised function FUNC.
@@ -547,6 +521,311 @@ If FUNC is not the symbol of an advised function, just returns FUNC."
     (when (not active)
         (and (fboundp 'deactivate-mark) (deactivate-mark))
         (and (fboundp 'zmacs-deactivate-region) (zmacs-deactivate-region)))))
+
+;; http://www.loveshack.ukfsn.org/emacs/syntax.el
+
+;;; syntax.el --- helper functions to find syntactic context
+
+;; Copyright (C) 2000, 2001, 2002, 2003, 2004,
+;;   2005, 2006 Free Software Foundation, Inc.
+
+;; Maintainer: FSF
+;; Keywords: internal
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; The main exported function is `cq-syntax-ppss'.  You might also need
+;; to call `cq-syntax-ppss-flush-cache' or to add it to
+;; `before-change-functions'(although this is automatically done by
+;; cq-syntax-ppss when needed, but that might fail if cq-syntax-ppss is
+;; called in a context where before-change-functions is temporarily
+;; let-bound to nil).
+
+;;; Todo:
+
+;; - do something about the case where the syntax-table is changed.
+;;   This typically happens with tex-mode and its `$' operator.
+;; - move font-lock-syntactic-keywords in here.  Then again, maybe not.
+;; - new functions `syntax-state', ... to replace uses of parse-partial-state
+;;   with something higher-level (similar to cq-syntax-ppss-context).
+;; - interaction with mmm-mode.
+
+;;; Code:
+
+;; Note: PPSS stands for `parse-partial-sexp state'
+
+(eval-when-compile (require 'cl))
+
+(defvar cq-font-lock-beginning-of-syntax-function)
+
+(defsubst cq-syntax-ppss-depth (ppss)
+  (nth 0 ppss))
+
+(defsubst cq-syntax-ppss-context (ppss)
+  "Return non-nil if syntactic state PPSS means in a string or comment.
+Return `string' or `comment' as appropriate, otherwise nil.
+PPSS is a list as returned by `parse-partial-sexp'."
+  (cond
+   ((nth 3 ppss) 'string)
+   ((nth 4 ppss) 'comment)
+   (t nil)))
+
+(defvar cq-syntax-ppss-max-span 20000
+  "Threshold below which cache info is deemed unnecessary.
+We try to make sure that cache entries are at least this far apart
+from each other, to avoid keeping too much useless info.")
+
+(defvar cq-syntax-begin-function nil
+  "Function to move back outside of any comment/string/paren.
+This function should move the cursor back to some syntactically safe
+point (where the PPSS is equivalent to nil).")
+
+(defvar cq-syntax-ppss-cache nil
+  "List of (POS . PPSS) pairs, in decreasing POS order.")
+(make-variable-buffer-local 'cq-syntax-ppss-cache)
+(defvar cq-syntax-ppss-last nil
+  "Cache of (LAST-POS . LAST-PPSS).")
+(make-variable-buffer-local 'cq-syntax-ppss-last)
+
+(defalias 'cq-syntax-ppss-after-change-function 'cq-syntax-ppss-flush-cache)
+(defun cq-syntax-ppss-flush-cache (beg &rest ignored)
+  "Flush the cache of `cq-syntax-ppss' starting at position BEG."
+  ;; Flush invalid cache entries.
+  (while (and cq-syntax-ppss-cache (> (caar cq-syntax-ppss-cache) beg))
+    (setq cq-syntax-ppss-cache (cdr cq-syntax-ppss-cache)))
+  ;; Throw away `last' value if made invalid.
+  (when (< beg (or (car cq-syntax-ppss-last) 0))
+    ;; If cq-syntax-begin-function jumped to BEG, then the old state at BEG can
+    ;; depend on the text after BEG (which is presumably changed).  So if
+    ;; BEG=(car (nth 10 cq-syntax-ppss-last)) don't reuse that data because the
+    ;; assumed nil state at BEG may not be valid any more.
+    (if (<= beg (or (car (nth 10 cq-syntax-ppss-last))
+                    (nth 9 cq-syntax-ppss-last)
+                    (nth 3 cq-syntax-ppss-last)
+                    0))
+	(setq cq-syntax-ppss-last nil)
+      (setcar cq-syntax-ppss-last nil)))
+  ;; Unregister if there's no cache left.  Sadly this doesn't work
+  ;; because `before-change-functions' is temporarily bound to nil here.
+  ;; (unless cq-syntax-ppss-cache
+  ;;   (remove-hook 'before-change-functions 'cq-syntax-ppss-flush-cache t))
+  )
+
+(defvar cq-syntax-ppss-stats
+  [(0 . 0.0) (0 . 0.0) (0 . 0.0) (0 . 0.0) (0 . 0.0) (1 . 2500.0)])
+(defun cq-syntax-ppss-stats ()
+  (mapcar (lambda (x)
+	    (condition-case nil
+		(cons (car x) (truncate (/ (cdr x) (car x))))
+	      (error nil)))
+	  cq-syntax-ppss-stats))
+
+(defun cq-syntax-ppss (&optional pos)
+  "Parse-Partial-Sexp State at POS.
+The returned value is the same as `parse-partial-sexp' except that
+the 2nd and 6th values of the returned state cannot be relied upon.
+Point is at POS when this function returns.
+
+The match data may be altered through running `cq-syntax-begin-function' (or
+`cq-font-lock-beginning-of-syntax-function')."
+  ;; Default values.
+  (unless pos (setq pos (point)))
+  ;;
+  (let ((old-ppss (cdr cq-syntax-ppss-last))
+	(old-pos (car cq-syntax-ppss-last))
+	(ppss nil)
+	(pt-min (point-min)))
+    (if (and old-pos (> old-pos pos)) (setq old-pos nil))
+    ;; Use the OLD-POS if usable and close.  Don't update the `last' cache.
+    (condition-case nil
+	(if (and old-pos (< (- pos old-pos)
+			    ;; The time to use cq-syntax-begin-function and
+			    ;; find PPSS is assumed to be about 2 * distance.
+			    (* 2 (/ (cdr (aref cq-syntax-ppss-stats 5))
+				    (1+ (car (aref cq-syntax-ppss-stats 5)))))))
+	    (progn
+	      (incf (car (aref cq-syntax-ppss-stats 0)))
+	      (incf (cdr (aref cq-syntax-ppss-stats 0)) (- pos old-pos))
+	      (parse-partial-sexp old-pos pos nil nil old-ppss))
+
+	  (cond
+	   ;; Use OLD-PPSS if possible and close enough.
+	   ((and (not old-pos) old-ppss
+		 ;; BEWARE! We rely on the undocumented 9th field.  The 9th
+		 ;; field currently contains the list of positions of
+		 ;; open-parens of the enclosing parens.  I.e. those
+		 ;; positions are outside of any string/comment
+		 ;; and the first of those is outside of any paren
+		 ;; (i.e. corresponds to a nil ppss).  If this list is empty
+		 ;; but we are in a string or comment, then the 8th field
+		 ;; contains a similar "toplevel" position.  If `pt-min' is
+		 ;; too far from `pos', we could try to use other positions
+		 ;; in (nth 9 old-ppss), but that doesn't seem to happen in
+		 ;; practice and it would complicate this code (and the
+		 ;; before-change-function code even more).  But maybe it
+		 ;; would be useful in "degenerate" cases such as when the
+		 ;; whole file is wrapped in a set of parentheses.
+		 (setq pt-min (or (car (nth 9 old-ppss))
+				  (nth 8 old-ppss)
+				  (nth 2 old-ppss)))
+		 (<= pt-min pos) (< (- pos pt-min) cq-syntax-ppss-max-span))
+	    (incf (car (aref cq-syntax-ppss-stats 1)))
+	    (incf (cdr (aref cq-syntax-ppss-stats 1)) (- pos pt-min))
+	    (setq ppss (parse-partial-sexp pt-min pos)))
+	   ;; The OLD-* data can't be used.  Consult the cache.
+	   (t
+	    (let ((cache-pred nil)
+		  (cache cq-syntax-ppss-cache)
+		  (pt-min (point-min))
+		  ;; I differentiate between PT-MIN and PT-BEST because
+		  ;; I feel like it might be important to ensure that the
+		  ;; cache is only filled with 100% sure data (whereas
+		  ;; cq-syntax-begin-function might return incorrect data).
+		  ;; Maybe that's just stupid.
+		  (pt-best (point-min))
+		  (ppss-best nil))
+	      ;; look for a usable cache entry.
+	      (while (and cache (< pos (caar cache)))
+		(setq cache-pred cache)
+		(setq cache (cdr cache)))
+	      (if cache (setq pt-min (caar cache) ppss (cdar cache)))
+
+	      ;; Setup the before-change function if necessary.
+	      (unless (or cq-syntax-ppss-cache cq-syntax-ppss-last)
+		(add-hook 'before-change-functions
+			  'cq-syntax-ppss-flush-cache t t))
+
+	      ;; Use the best of OLD-POS and CACHE.
+	      (if (or (not old-pos) (< old-pos pt-min))
+		  (setq pt-best pt-min ppss-best ppss)
+		(incf (car (aref cq-syntax-ppss-stats 4)))
+		(incf (cdr (aref cq-syntax-ppss-stats 4)) (- pos old-pos))
+		(setq pt-best old-pos ppss-best old-ppss))
+
+	      ;; Use the `cq-syntax-begin-function' if available.
+	      ;; We could try using that function earlier, but:
+	      ;; - The result might not be 100% reliable, so it's better to use
+	      ;;   the cache if available.
+	      ;; - The function might be slow.
+	      ;; - If this function almost always finds a safe nearby spot,
+	      ;;   the cache won't be populated, so consulting it is cheap.
+	      (when (and (not cq-syntax-begin-function)
+			 (if (boundp 'cq-font-lock-beginning-of-syntax-function)
+			     cq-font-lock-beginning-of-syntax-function))
+		(set (make-local-variable 'cq-syntax-begin-function)
+		     cq-font-lock-beginning-of-syntax-function))
+	      (when (and cq-syntax-begin-function
+			 (progn (goto-char pos)
+				(funcall cq-syntax-begin-function)
+				;; Make sure it's better.
+				(> (point) pt-best))
+			 ;; Simple sanity check.
+			 (not (memq (get-text-property (point) 'face)
+				    '(font-lock-string-face font-lock-doc-face
+				      font-lock-comment-face))))
+		(incf (car (aref cq-syntax-ppss-stats 5)))
+		(incf (cdr (aref cq-syntax-ppss-stats 5)) (- pos (point)))
+		(setq pt-best (point) ppss-best nil))
+
+	      (cond
+	       ;; Quick case when we found a nearby pos.
+	       ((< (- pos pt-best) cq-syntax-ppss-max-span)
+		(incf (car (aref cq-syntax-ppss-stats 2)))
+		(incf (cdr (aref cq-syntax-ppss-stats 2)) (- pos pt-best))
+		(setq ppss (parse-partial-sexp pt-best pos nil nil ppss-best)))
+	       ;; Slow case: compute the state from some known position and
+	       ;; populate the cache so we won't need to do it again soon.
+	       (t
+		(incf (car (aref cq-syntax-ppss-stats 3)))
+		(incf (cdr (aref cq-syntax-ppss-stats 3)) (- pos pt-min))
+
+		;; If `pt-min' is too far, add a few intermediate entries.
+		(while (> (- pos pt-min) (* 2 cq-syntax-ppss-max-span))
+		  (setq ppss (parse-partial-sexp
+			      pt-min (setq pt-min (/ (+ pt-min pos) 2))
+			      nil nil ppss))
+		  (let ((pair (cons pt-min ppss)))
+		    (if cache-pred
+			(push pair (cdr cache-pred))
+		      (push pair cq-syntax-ppss-cache))))
+
+		;; Compute the actual return value.
+		(setq ppss (parse-partial-sexp pt-min pos nil nil ppss))
+
+		;; Debugging check.
+		;; (let ((real-ppss (parse-partial-sexp (point-min) pos)))
+		;;   (setcar (last ppss 4) 0)
+		;;   (setcar (last real-ppss 4) 0)
+		;;   (setcar (last ppss 8) nil)
+		;;   (setcar (last real-ppss 8) nil)
+		;;   (unless (equal ppss real-ppss)
+		;;     (message "!!Syntax: %s != %s" ppss real-ppss)
+		;;     (setq ppss real-ppss)))
+
+		;; Store it in the cache.
+		(let ((pair (cons pos ppss)))
+		  (if cache-pred
+		      (if (> (- (caar cache-pred) pos) cq-syntax-ppss-max-span)
+			  (push pair (cdr cache-pred))
+			(setcar cache-pred pair))
+		    (if (or (null cq-syntax-ppss-cache)
+			    (> (- (caar cq-syntax-ppss-cache) pos)
+			       cq-syntax-ppss-max-span))
+			(push pair cq-syntax-ppss-cache)
+		      (setcar cq-syntax-ppss-cache pair)))))))))
+
+	  (setq cq-syntax-ppss-last (cons pos ppss))
+	  ppss)
+      (args-out-of-range
+       ;; If the buffer is more narrowed than when we built the cache,
+       ;; we may end up calling parse-partial-sexp with a position before
+       ;; point-min.  In that case, just parse from point-min assuming
+       ;; a nil state.
+       (parse-partial-sexp (point-min) pos)))))
+
+;; Debugging functions
+
+(defun cq-syntax-ppss-debug ()
+  (let ((pt nil)
+	(min-diffs nil))
+    (dolist (x (append cq-syntax-ppss-cache (list (cons (point-min) nil))))
+      (when pt (push (- pt (car x)) min-diffs))
+      (setq pt (car x)))
+    min-diffs))
+
+;; XEmacs compatibility functions
+
+;; (defun buffer-syntactic-context (&optional buffer)
+;;   "Syntactic context at point in BUFFER.
+;; Either of `string', `comment' or `nil'.
+;; This is an XEmacs compatibility function."
+;;   (with-current-buffer (or buffer (current-buffer))
+;;     (cq-syntax-ppss-context (cq-syntax-ppss))))
+
+;; (defun buffer-syntactic-context-depth (&optional buffer)
+;;   "Syntactic parenthesis depth at point in BUFFER.
+;; This is an XEmacs compatibility function."
+;;   (with-current-buffer (or buffer (current-buffer))
+;;     (cq-syntax-ppss-depth (cq-syntax-ppss))))
+
+;; (provide 'syntax)
 
 (provide 'c-quick-2)
 ;;; c-quick-2.el ends here
