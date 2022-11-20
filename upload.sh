41b6907cc4c2746afd4b5fb7e7d895b1408247d2
JavaCommons Technologies <javacommons@gmail.com>
2022-11-20 16:58:14 +0900

    .

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
-gh auth login --hostname github.com --git-protocol https --web
-cp -v ~/elisp/c-quick/c-quick.el .
+#! busybox bash
+#gh auth login --hostname github.com --git-protocol https --web
+#cp -v ~/elisp/c-quick/c-quick.el .
+cp -v ~/c-quick.el .
 gh.exe release upload package c-quick.el --clobber
 git-put.cmd
-rm -rf ~/elisp/c-quick
+#rm -rf ~/elisp/c-quick
